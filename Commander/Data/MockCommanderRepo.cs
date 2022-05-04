d4073a2a71da7cecf39082784b1953ae203c4b52
Alcides Polcanco <20198294@itla.edu.do>
2022-05-04 13:17:52 -0400

    Agregar archivos de proyecto.

---
+++ Commander/Data/MockCommanderRepo.cs
@@ -0,0 +1,44 @@
+﻿using Commander.Models;
+using System.Collections.Generic;
+
+namespace Commander.Data
+{
+    {
+        public void CreateCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            var commands = new List<Command>()
+            {
+                new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "kettle & Pan" },
+                new Command { Id = 1, HowTo = "Cut bread", Line = "Get a knife", Platform = "knife & chopping board" },
+                new Command { Id = 3, HowTo = "Make cup of tea", Line = "Place teabag in cup", Platform = "kettle & Pan" }
+            };
+            return commands;
+        }
+
+        public Command GetCommandById(int Id)
+        {
+            return new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "kettle & Pan" };
+        }
+
+        public bool SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+   
+}
